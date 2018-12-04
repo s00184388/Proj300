5a9929f1ec6a0a18fa3aa09dccded1292fc743f0
S00184388 <S00184388@mail.itsligo.ie>
2018-12-04 10:37:18 +0000

    Wishlist connected to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,23 @@
-import firebase from 'firebase';
+import firebase from "firebase";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBUYWmaD070queqzty2IDqiqJIdQ12jcK4",
-    authDomain: "kudoshealth-2961f.firebaseapp.com",
-    databaseURL: "https://kudoshealth-2961f.firebaseio.com",
-    projectId: "kudoshealth-2961f",
-    storageBucket: "kudoshealth-2961f.appspot.com",
-    messagingSenderId: "62840631243"
-  };
+  apiKey: "AIzaSyBUYWmaD070queqzty2IDqiqJIdQ12jcK4",
+  authDomain: "kudoshealth-2961f.firebaseapp.com",
+  databaseURL: "https://kudoshealth-2961f.firebaseio.com",
+  projectId: "kudoshealth-2961f",
+  storageBucket: "kudoshealth-2961f.appspot.com",
+  messagingSenderId: "62840631243"
+};
 
-  const fire = firebase.initializeApp(config)
-  export { fire }
+const fire = firebase.initializeApp(config);
+//this.auth = app.auth();
+
+// *** Auth API ***//
+
+/*doCreateUserWithEmailAndPassword = (email, password) =>
+  this.auth.doCreateUserWithEmailAndPassword(email, password);
+
+doSignInWithEmailAndPassword = (email, password) =>
+  this.auth.doSignInWithEmailAndPassword(email, password);*/
+export { fire };
