fd96ff45a48af2dff5f49cb118747b40a636e818
s00190970 <s00190970@mail.itsligo.ie>
2019-03-19 19:18:13 +0000

    code comment by Cristian

--- src/Pages/Brands.js
+++ src/Pages/Brands.js
@@ -6,6 +6,8 @@ import "./CssPages/Brands.css";
 
 const firebaseServices = new FirebaseServices();
 
+// component that renders a brand card
+// brand cards can be clicked and the employee will be redirrected to the brand's shop
 class BrandCard extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +17,7 @@ class BrandCard extends Component {
     const brandName = brand.name;
     const brandPicture = brand.picture;
     const brandDescription = brand.description;
+    //style for the image
     const style = {
       width: 128,
       height: 128
@@ -40,12 +43,16 @@ class BrandCard extends Component {
   }
 }
 
+//component that renders all the brand cards
+//this page has a list of brands in form of cards
 export class Brands extends Component {
   constructor(props) {
     super(props);
     this.state = { brands: [], fetchInProgress: true };
     this.subscriptions = [];
   }
+
+  //getting all the brands
   componentDidMount() {
     this.setState({ fetchInProgress: true });
     this.subscriptions.push(
@@ -59,6 +66,7 @@ export class Brands extends Component {
   }
   render() {
     const brands = this.state.brands;
+    //forach brand, render the brand card
     const list = brands.map(br => <BrandCard brand={br} key={br.key} />);
     var fetchInProgress = this.state.fetchInProgress;
     return (
